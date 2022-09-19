2eb1a9676205d4ec6b1e85faa3ab4ef40a4cf36c
soul_hn <vldnrlf123@naver.com>
2022-09-19 14:48:06 +0900

    add solution

--- programmers/lv1/음양더하기.js
+++ programmers/lv1/음양더하기.js
@@ -1,15 +1,7 @@
-absolutes = [4, 7, 12];
-signs = [true, false, true];
-
 function solution(absolutes, signs) {
   for (let i = 0; i < signs.length; i++) {
     signs[i] === true ? (absolutes[i] = +absolutes[i]) : (absolutes[i] = -absolutes[i]);
-    console.log(signs[i]);
-    console.log(absolutes);
   }
   var answer = absolutes.reduce((acc, curr) => acc + curr);
-  console.log(answer);
-  // return answer;
+  return answer;
 }
-
-solution(absolutes, signs);
