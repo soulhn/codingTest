2e81a6bc4b829fd6aac456be5a1851413f10b851
soulhn <vldnrlf123@naver.com>
2022-10-05 11:49:19 +0900

    notebook

---
+++ programmers/lv1/소수만들기.js
@@ -0,0 +1,21 @@
+function isPrime(num) {
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+}
+
+function solution(nums) {
+  let count = 0;
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      for (let k = j + 1; k < nums.length; k++) {
+        if (isPrime(nums[k] + nums[j] + nums[i])) {
+          console.log(nums[k] + nums[j] + nums[i]);
+          count++;
+        }
+      }
+    }
+  }
+  return count;
+}
