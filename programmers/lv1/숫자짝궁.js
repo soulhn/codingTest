8ef10e9d875b237b65d8d05255638f042b435f78
soulhn <vldnrlf123@naver.com>
2022-10-09 18:32:51 +0900

    notebook

--- programmers/lv1/숫자짝궁.js
+++ programmers/lv1/숫자짝궁.js
@@ -1,25 +1,17 @@
-let X = "100";
-let Y = "203045";
-// 미해결..
 function solution(X, Y) {
   let answer = [];
-  X = X.split("");
-  Y = Y.split("");
-  for (let i = 0; i < X.length; i++) {
-    for (let j = 0; j < Y.length; j++) {
-      if (X[i] === Y[j]) {
-        answer.push(X[i]);
-        Y.split(j, 1);
-        break;
-      }
+  const setX = new Set(X);
+  const setY = new Set(Y);
+  const intersection = [...setX].filter((item) => setY.has(item));
+  for (const i of intersection) {
+    let minNum = Math.min([...X].filter((a) => a === i).length, [...Y].filter((a) => a === i).length);
+    for (let j = 0; j < minNum; j++) {
+      answer.push(i);
     }
   }
   if (answer.length === 0) return "-1";
-  // console.log(answer);
 
   answer = answer.map((item) => parseInt(item, 10)).sort((a, b) => b - a);
-  // .join("");
-  console.log(answer);
 
   return answer.every((item) => item === 0) ? "0" : answer.join("");
 }
