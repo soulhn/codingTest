8135e6b5188579217c5a2f69ebaf42de6f4c5083
soulhn <vldnrlf123@naver.com>
2022-10-26 00:06:25 +0900

    스택 풀이 추가

--- programmers/a_z_js/스택올바른괄호실습.js
+++ programmers/a_z_js/스택올바른괄호실습.js
@@ -7,3 +7,37 @@ function solution(s) {
   }
   return answer.length === 0 ? true : false;
 }
+
+//강사님 코드
+function solution2(s) {
+  const stack = [];
+
+  for (const c of s) {
+    if (c === "(") {
+      stack.push(c);
+    } else {
+      if (stack.length === 0) {
+        return false;
+      }
+      stack.pop();
+    }
+  }
+  return stack.length === 0;
+}
+
+// 강사님 스택의 개념을 이용하지만 배열 사용 X
+function solution3(s) {
+  let count = 0;
+
+  for (const c of s) {
+    if (c === "(") {
+      count += 1;
+    } else {
+      if (stack.length === 0) {
+        return false;
+      }
+      count -= 1;
+    }
+  }
+  return count === 0;
+}
