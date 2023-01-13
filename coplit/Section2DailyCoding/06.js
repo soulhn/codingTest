eb92856882682a5110837408d1c8c8f6c66bf182
soulhn <vldnrlf123@naver.com>
2023-01-14 03:13:46 +0900

    add solution

--- coplit/Section2DailyCoding/06.js
+++ coplit/Section2DailyCoding/06.js
@@ -1,13 +1,8 @@
-let str = "javascript  is sexy ";
 function letterCapitalize(str) {
-  // TODO: 여기에 코드를 작성합니다.
-}
-
-console.log(
-  str
+  return str
     .split(" ")
     .map((a) => {
       return a[0] ? a[0].toUpperCase() + a.slice(1) : "";
     })
-    .join(" ")
-);
+    .join(" ");
+}
