789812433338f6bf728af9f992036cdaf9442150
soulhn <vldnrlf123@naver.com>
2023-01-17 23:04:33 +0900

    Add solution

--- coplit/Section2DailyCoding/11.js
+++ coplit/Section2DailyCoding/11.js
@@ -1,23 +1,18 @@
-let arr = ["where", "is", "the", "longest", "word"];
 function removeExtremes(arr) {
   const copyArr = arr;
   let short = 0;
   let long = 0;
 
   for (let i = 1; i < arr.length; i++) {
-    console.log(arr[i]);
-    if (arr[short] >= arr[i]) {
+    if (arr[short].length >= arr[i].length) {
       short = i;
     }
-    if (arr[long] <= arr[i]) {
+    if (arr[long].length <= arr[i].length) {
       long = i;
     }
   }
   copyArr.splice(long, 1, false);
-
   copyArr.splice(short, 1, false);
 
   return copyArr.filter((el) => el !== false);
 }
-
-console.log("결과는 : " + removeExtremes(arr));
