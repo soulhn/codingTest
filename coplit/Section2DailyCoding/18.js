dde38a75d853fc789e5aa030daff00c024006894
soulhn <vldnrlf123@naver.com>
2023-01-22 00:58:03 +0900

    Add solution

--- coplit/Section2DailyCoding/18.js
+++ coplit/Section2DailyCoding/18.js
@@ -1,7 +1,5 @@
-let str = "Hello6 ";
 function numberSearch(str) {
   if (str === "") return 0;
-  // TODO: 여기에 코드를 작성합니다.
   const numArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
   let strLength = 0;
   let result = 0;
@@ -12,9 +10,5 @@ function numberSearch(str) {
       strLength++;
     }
   }
-  console.log(result);
-  console.log(strLength);
-  return Math.ceil(result / strLength);
+  return Math.round(result / strLength);
 }
-
-console.log(numberSearch(str));
