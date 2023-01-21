f582f02aeceb6bcb38efb0cd011ff7ab322ca61e
soulhn <vldnrlf123@naver.com>
2023-01-21 23:58:29 +0900

    Add reference code

--- coplit/Section2DailyCoding/16.js
+++ coplit/Section2DailyCoding/16.js
@@ -11,15 +11,15 @@ function isIsogram(str) {
     return true;
   }
 
-  let cache = {};
-  let strLowered = str.toLowerCase();
+  let cache = {}; //빈객체
+  let strLowered = str.toLowerCase(); //대소문자 구별을 하지 않아 모두 소문자로
 
   for (let i = 0; i < strLowered.length; i++) {
     if (cache[strLowered[i]]) {
-      return false;
+      //cache의 키 값이 있다면
+      return false; //있으면 false
     }
-    cache[strLowered[i]] = true;
+    cache[strLowered[i]] = true; //처음 등장한 소문자는 cache에 키값으로 true 저장
   }
-
-  return true;
+  return true; // 중복 값 체크 조건문에 걸리지 않고 for문 종료시 true 반환
 }
