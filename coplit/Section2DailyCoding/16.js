18e1ec5ddd6380f173054029520394ce3f77deb9
soulhn <vldnrlf123@naver.com>
2023-01-21 23:25:38 +0900

    Add reference code

--- coplit/Section2DailyCoding/16.js
+++ coplit/Section2DailyCoding/16.js
@@ -4,3 +4,22 @@ function isIsogram(str) {
   const { size } = new Set(upperStr);
   return size === str.length;
 }
+
+//래퍼런스 코드
+function isIsogram(str) {
+  if (str.length === 0) {
+    return true;
+  }
+
+  let cache = {};
+  let strLowered = str.toLowerCase();
+
+  for (let i = 0; i < strLowered.length; i++) {
+    if (cache[strLowered[i]]) {
+      return false;
+    }
+    cache[strLowered[i]] = true;
+  }
+
+  return true;
+}
