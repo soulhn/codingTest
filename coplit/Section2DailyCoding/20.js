03b30c54f7293e232a1c20ad52807d75d4ff8f68
soulhn <vldnrlf123@naver.com>
2023-01-23 20:45:31 +0900

    Add solution Daily20

--- coplit/Section2DailyCoding/20.js
+++ coplit/Section2DailyCoding/20.js
@@ -1,14 +1,9 @@
-let str = "wwwggoppopppp";
-//3wggoppo4p
-//3wggoppo
-
 //문자열 순회
 //이전 문자와 현재 문자가 같은지 확인
 // => 같다면 카운트 +1
-
 // => 다르면 3이상 일 경우 숫자, 키운트 0으로 초기화, 이전 글자 변경
 // => 2이하 일 경우 , 이전 글자를 count 만큼 정답 배열에 추가
-
+// 첫번째 풀이
 function compressString(str) {
   let count = 1; //초기 값(맨 앞글자 수)
   let beforeLetter = str[0]; //맨 앞 글자
@@ -37,5 +32,3 @@ function compressString(str) {
   }
   return answer;
 }
-
-console.log(compressString(str));
