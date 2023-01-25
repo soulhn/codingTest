3748fad59ae2adebb47e70a67e1038e82b567dbc
soulhn <vldnrlf123@naver.com>
2023-01-25 23:17:26 +0900

    Add 짝지어 제거하기 Ref 코드

--- 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.js
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -9,6 +9,31 @@ function solution(s) {
       stackArr.pop();
     }
   }
+  return stackArr.length === 0 ? 1 : 0;
+}
 
-  return stackArr.length === 0 ? 1: 0;
+//다른 사람 풀이
+function solution(s) {
+  let arr = [];
+  for (let i = 0; i < s.length; i++) {
+    if (arr[arr.length - 1] !== s[i]) {
+      arr.push(s[i]);
+    } else {
+      arr.pop();
+    }
+  }
+  return arr.length > 0 ? 0 : 1;
+}
+
+//다른 사람 풀이2
+function solution(s) {
+  let arr = [];
+  for (let i = 0; i < s.length; i++) {
+    arr.push(s[i]);
+    if (arr[arr.length - 1] === arr[arr.length - 2]) {
+      arr.pop();
+      arr.pop();
+    }
+  }
+  return arr.length === 0 ? 1 : 0;
 }
