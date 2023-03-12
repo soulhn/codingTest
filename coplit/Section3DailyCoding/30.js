ac67e4e57313d54e51b31a3a984b47f4621e70d0
soulhn <vldnrlf123@naver.com>
2023-03-12 13:09:40 +0900

    feat: 30 solution

---
+++ coplit/Section3DailyCoding/30.js
@@ -0,0 +1,23 @@
+const balancedBrackets = function (str) {
+  const stack = [];
+  const opener = {
+    "{": "}",
+    "[": "]",
+    "(": ")",
+  };
+  const closer = "}])";
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] in opener) {
+      stack.push(str[i]);
+    } else if (closer.includes(str[i])) {
+      const top = stack.pop();
+      const pair = opener[top];
+      if (pair !== str[i]) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
+};
