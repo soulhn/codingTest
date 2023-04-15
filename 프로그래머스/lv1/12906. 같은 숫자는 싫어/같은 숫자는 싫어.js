154f48a139d69a73bd52cedf706e5d9a8387d3cf
soulhn <88018456+soulhn@users.noreply.github.com>
2023-04-15 22:29:14 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 22.75 ms, Memory: 83.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,3 +1,10 @@
-function solution(arr) {
-  return arr.filter((a, i) => a !== arr[i + 1]);
+function solution(arr)
+{
+    const answer = []
+    arr.forEach(el => {
+        if(answer[answer.length-1] !== el) {
+            answer.push(el)
+        }
+    })
+    return answer
 }
