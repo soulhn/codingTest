49611b4bb5d9a4bc68700f8fbb03e41a24046d29
soulhn <88018456+soulhn@users.noreply.github.com>
2023-04-22 19:23:59 +0900

    [unrated] Title: 홀짝 구분하기, Time: 72.32 ms, Memory: 32.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181944. 홀짝 구분하기/홀짝 구분하기.js
@@ -0,0 +1,16 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = line.split(' ');
+}).on('close', function () {
+    n = Number(input[0]);
+    const isOdd = n % 2 === 0 ? "even" : "odd"
+    console.log(`${n} is ${isOdd}`)
+    
+});
