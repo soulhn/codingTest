7131546c2c575f556008e9a888f226c8a10b82be
soulhn <88018456+soulhn@users.noreply.github.com>
2023-06-02 21:34:07 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.22 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.js
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -1,9 +1,21 @@
+// function solution(n) {
+//     let answer, t1 = 0, t2 = 1;
+//     for(let i = 2 ; i <= n + 1 ; i++) {
+//         answer = t1 + t2 % 1234567;
+//         t1 = t2;
+//         t2 = answer;
+//     }
+//     return answer % 1234567;
+// }
+
 function solution(n) {
-    let answer, t1 = 0, t2 = 1;
-    for(let i = 2 ; i <= n + 1 ; i++) {
-        answer = t1 + t2 % 1234567;
-        t1 = t2;
-        t2 = answer;
+    var answer = 0;
+    var dp=[];
+    dp[1]=1;
+    dp[2]=2;
+    for(var i=3;i<=n;i++){
+        dp[i]=dp[i-1]+dp[i-2] %1234567;
     }
-    return answer % 1234567;
+    answer=dp[n];
+    return answer%1234567;
 }
