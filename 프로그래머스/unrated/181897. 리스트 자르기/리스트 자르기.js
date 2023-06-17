f22c051c32f623c4205d74e04e2245798e88fd57
soulhn <88018456+soulhn@users.noreply.github.com>
2023-06-17 21:24:07 +0900

    [unrated] Title: 리스트 자르기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181897. 리스트 자르기/리스트 자르기.js
@@ -0,0 +1,17 @@
+function solution(n, slicer, num_list) {
+    //정수 n, n에 따라 분기
+    //정수 3개 : slicer
+    //리스트 : num_list
+    switch(n){
+        case 1:
+            return num_list.slice(0, slicer[1]+1)
+        case 2:
+            return num_list.slice(slicer[0])
+        case 3:
+            return num_list.slice(slicer[0],slicer[1]+1)
+        case 4:
+            return num_list.slice(slicer[0],slicer[1]+1).filter((_, idx) => idx % slicer[2] === 0)
+    }
+ 
+    
+}
